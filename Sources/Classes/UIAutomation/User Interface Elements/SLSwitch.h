054d6b683b5afb90584d55ff1dfe2c26d40e0568
Justin Mutter <mutter.justin@gmail.com>
2013-09-13 16:21:42 -0400

    WIP - SLSwitch

---
+++ Sources/Classes/UIAutomation/User Interface Elements/SLSwitch.h
@@ -0,0 +1,15 @@
+//
+//  SLSwitch.h
+//  Subliminal
+//
+//  Created by Justin Mutter on 2013-09-13.
+//  Copyright (c) 2013 Inkling. All rights reserved.
+//
+
+#import <Subliminal/Subliminal.h>
+
+@interface SLSwitch : SLButton
+
+- (void)setValue:(BOOL)value;
+
+@end
---
+++ Sources/Classes/UIAutomation/User Interface Elements/SLSwitch.m
@@ -0,0 +1,20 @@
+//
+//  SLSwitch.m
+//  Subliminal
+//
+//  Created by Justin Mutter on 2013-09-13.
+//  Copyright (c) 2013 Inkling. All rights reserved.
+//
+
+#import "SLSwitch.h"
+#import "SLUIAElement+Subclassing.h"
+
+@implementation SLSwitch
+
+- (void)setValue:(BOOL)value
+{
+    NSString *valueString = value ? @"true" : @"false";
+    [self waitUntilTappable:NO thenSendMessage:@"setValue(%@)", valueString];
+}
+
+@end
